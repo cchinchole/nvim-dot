8c26a6b8f709d41e7d9b25dac846d0e759f7907e
cchinchole <79995419+cchinchole@users.noreply.github.com>
2025-06-26 04:05:56 -0500

    Changes Disabled lualine Disabled dap Changed LSP a bit Enforced 4 space tabbing Added keybinds for movement and deletion

---
+++ lua/keybinds.lua
@@ -0,0 +1,51 @@
+local opts = { noremap = true, silent = true }
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Moving visual selection
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "moves lines down in visual selection" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "moves lines up in visual selection" })
+
+-- Move selection horizontally
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
+
+--Join lines 
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Jump half page with centering
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "move down in buffer with cursor centered" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "move up in buffer with cursor centered" })
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Remove sticking to clipboard when deleting
+vim.keymap.set("n", "x", '"_x', opts)
+
+-- remember yanked
+vim.keymap.set("v", "p", '"_dp', opts)  
+
+-- Yank to system clipbaord
+vim.keymap.set("n", "<leader>Y", [["+Y]], opts)
+
+-- Stop highlighting and escape insert
+vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("n", "<C-c>", ":nohl<CR>", { desc = "Clear search hl", silent = true })
+
+-- Disable Q
+vim.keymap.set("n", "Q", "<nop>")
+
+-- Formatting
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- Toggle LSP diagnostics visibility
+local isLspDiagnosticsVisible = true
+vim.keymap.set("n", "<leader>lx", function()
+    isLspDiagnosticsVisible = not isLspDiagnosticsVisible
+    vim.diagnostic.config({
+        virtual_text = isLspDiagnosticsVisible,
+        underline = isLspDiagnosticsVisible
+    })
+end, { desc = "Toggle LSP diagnostics" })
---
+++ lua/options.lua
@@ -0,0 +1,19 @@
+vim.opt.guicursor=""
+vim.opt.nu = true
+vim.wo.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.incsearch = true
+vim.opt.updatetime = 5
+
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+
+vim.opt.hlsearch = true
+
+
+vim.g.editorconfig = true
